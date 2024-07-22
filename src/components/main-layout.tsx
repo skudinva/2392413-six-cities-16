80ad0460734341eefe8c0349461915a9d9adb13c
Skudin Vladimir <skudinva@gmail.com>
2024-07-23 00:36:47 +0700

    Линк++

--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import { AppRoute } from '../const';
 import SignUser from './sign-user';
 
@@ -11,7 +11,10 @@ function MainLayout(): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
+              <Link
+                className="header__logo-link header__logo-link--active"
+                to={AppRoute.Main}
+              >
                 <img
                   className="header__logo"
                   src="img/logo.svg"
@@ -19,7 +22,7 @@ function MainLayout(): JSX.Element {
                   width="81"
                   height="41"
                 />
-              </a>
+              </Link>
             </div>
             {!isLoginForm && <SignUser />}
           </div>
