a1c0bcf74938b16c634f8df1a1660853543be098
Skudin Vladimir <skudinva@gmail.com>
2024-07-23 21:03:36 +0700

    Так короче

--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -4,7 +4,7 @@ import SignUser from './sign-user';
 
 function MainLayout(): JSX.Element {
   const { pathname } = useLocation();
-  const isLoginForm = pathname === (AppRoute.Login as string);
+  const isLoginForm = pathname === String(AppRoute.Login);
   return (
     <div className="page page--gray page--main">
       <header className="header">
