988dadeefa0a991ec00532bc8fd9d17278184014
Skudin Vladimir <skudinva@gmail.com>
2024-07-24 09:38:45 +0700

    Обработал отрисовку пустого списка офферов

--- src/components/no-places.tsx
+++ src/components/no-places.tsx
@@ -1,6 +1,6 @@
 function NoPlaces(): JSX.Element {
   return (
-    <div className="cities__places-container cities__places-container--empty container">
+    <div className="cities__places-container container cities__places-container--empty">
       <section className="cities__no-places">
         <div className="cities__status-wrapper tabs__content">
           <b className="cities__status">No places to stay available</b>
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,6 +1,8 @@
+import classNames from 'classnames';
 import { useState } from 'react';
 import CityList from '../components/city-list';
 import Map from '../components/map';
+import NoPlaces from '../components/no-places';
 import OfferSort from '../components/offer-sort';
 import PlaceCardList from '../components/place-card-list';
 import { CityName } from '../const';
@@ -13,7 +15,6 @@ function Main(): JSX.Element {
   const currentSort = useAppSelector((state) => state.currentSort);
   const offers = useAppSelector((state) => state.offers);
   const [currentOffer, setCurrentOffer] = useState<OfferEntity | null>(null);
-
   const cityOffers = getCityOffers(offers, currentCity);
   const sortFunction = applySorting[currentSort];
   if (sortFunction) {
@@ -21,7 +22,11 @@ function Main(): JSX.Element {
   }
 
   return (
-    <main className="page__main page__main--index">
+    <main
+      className={classNames('page__main', 'page__main--index', {
+        'page__main--index-empty': !offers.length,
+      })}
+    >
       <h1 className="visually-hidden">Cities</h1>
       <div className="tabs">
         <section className="locations container">
@@ -29,29 +34,33 @@ function Main(): JSX.Element {
         </section>
       </div>
       <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">
-              {currentCity
-                ? `${cityOffers.length} places to stay in ${currentCity.name}`
-                : ''}
-            </b>
-            <OfferSort />
-            <PlaceCardList
-              offers={cityOffers}
-              onActiveOfferChange={setCurrentOffer}
-            />
-          </section>
-          <div className="cities__right-section">
-            <Map
-              offers={cityOffers}
-              city={currentCity}
-              currentOffer={currentOffer}
-              baseClass="cities"
-            />
+        {cityOffers.length ? (
+          <div className="cities__places-container container">
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">
+                {currentCity
+                  ? `${cityOffers.length} places to stay in ${currentCity.name}`
+                  : ''}
+              </b>
+              <OfferSort />
+              <PlaceCardList
+                offers={cityOffers}
+                onActiveOfferChange={setCurrentOffer}
+              />
+            </section>
+            <div className="cities__right-section">
+              <Map
+                offers={cityOffers}
+                city={currentCity}
+                currentOffer={currentOffer}
+                baseClass="cities"
+              />
+            </div>
           </div>
-        </div>
+        ) : (
+          <NoPlaces />
+        )}
       </div>
     </main>
   );
