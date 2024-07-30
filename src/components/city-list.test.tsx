819f70fda11feb2f69ee543befab308ef2829f26
Skudin Vladimir <skudinva@gmail.com>
2024-07-30 10:18:56 +0700

    рефакторинг

--- src/components/city-list.test.tsx
+++ src/components/city-list.test.tsx
@@ -1,19 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { CityName } from '../const';
-import { withHistory, withStore } from '../utils/mock-component';
+import { renderWithStoreAndHistoryComponent } from '../utils/mock-component';
 import { makeFakeState } from '../utils/mocks';
 import CityList from './city-list';
 
 describe('Component: CityList', () => {
   it('should render correctly', () => {
     const state = makeFakeState();
-    const { withStoreComponent } = withStore(
-      <CityList cities={CityName} />,
-      state
-    );
-
-    const preparedComponent = withHistory(withStoreComponent);
-    render(preparedComponent);
+    renderWithStoreAndHistoryComponent(<CityList cities={CityName} />, state);
     Object.values(CityName).map((city) => {
       expect(screen.getByText(city)).toBeInTheDocument();
     });
