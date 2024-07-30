28d27073e1dffeefec6770f47a86e19321f664ab
Skudin Vladimir <skudinva@gmail.com>
2024-07-30 09:55:33 +0700

    Разбил на два теста

--- src/components/offer-review-form.test.tsx
+++ src/components/offer-review-form.test.tsx
@@ -5,13 +5,13 @@ import { makeFakeState } from '../utils/mocks';
 import OfferReviewForm from './offer-review-form';
 
 describe('Component: OfferReviewForm', () => {
-  it('should render & correctly button disable|enable ', async () => {
-    const state = makeFakeState();
-    const ratingElement = 'rating5Element';
-    const reviewElement = 'reviewElement';
-    const incorrectReviewValue = '12345';
-    const correctReviewValue = Array.from({ length: 60 }).join('1');
+  const ratingElement = 'rating5Element';
+  const reviewElement = 'reviewElement';
+  const incorrectReviewValue = '12345';
+  const correctReviewValue = Array.from({ length: 60 }).join('1');
+  const state = makeFakeState();
 
+  it('should render & button disable ', async () => {
     const { withStoreComponent } = withStore(<OfferReviewForm />, state);
     const preparedComponent = withHistory(withStoreComponent);
     render(preparedComponent);
@@ -24,8 +24,15 @@ describe('Component: OfferReviewForm', () => {
     );
     expect(screen.getByDisplayValue(incorrectReviewValue)).toBeInTheDocument();
     expect(button).toBeDisabled();
+  });
 
-    await userEvent.clear(screen.getByTestId(reviewElement));
+  it('should render & button enable ', async () => {
+    const { withStoreComponent } = withStore(<OfferReviewForm />, state);
+    const preparedComponent = withHistory(withStoreComponent);
+    render(preparedComponent);
+    const button = screen.getByRole('button');
+
+    await userEvent.click(screen.getByTestId(ratingElement));
     await userEvent.type(screen.getByTestId(reviewElement), correctReviewValue);
     expect(screen.getByDisplayValue(correctReviewValue)).toBeInTheDocument();
     expect(button).toBeEnabled();
